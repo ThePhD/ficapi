0b41f2b3dca905536a85442ff4eb86468b05bd4b
ThePhD <phdofthehouse@gmail.com>
2019-05-06 16:08:24 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ficapi - fictional C API
+
+A (fi)ctional (C) (API) for testing and benchmarking purposes, capable of providing both DLL-hidden and exposed, inline-optimize-able functions and data.
---
+++ include/ficapi/ficapi.hpp
@@ -0,0 +1,17 @@
+//  Copyright ⓒ 2018-2019 ThePhD.
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+//  See http://www.boost.org/libs/out_ptr/ for documentation.
+
+#pragma once
+
+#ifndef FICAPI_FICAPI_HPP
+#define FICAPI_FICAPI_HPP
+
+#include <ficapi/c_api.hpp>
+#include <ficapi/c++_api.hpp>
+
+#endif // FICAPI_FICAPI_HPP
